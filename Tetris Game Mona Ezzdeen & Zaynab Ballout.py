77eefe303006f7bf918d390765129bfa84887595
monaezz <monaezzdeen33@gmail.com>
2022-05-17 11:26:09 +0300

    declare colors and import libraries

---
+++ Tetris Game Mona Ezzdeen & Zaynab Ballout.py
@@ -0,0 +1,15 @@
+# importing from environments
+import pygame
+import random
+
+# declare colors
+colors = [
+    (0, 0, 0),
+    (120, 37, 179),
+    (100, 179, 179),
+    (80, 34, 22),
+    (80, 134, 22),
+    (180, 34, 22),
+    (180, 34, 122),
+]
+
