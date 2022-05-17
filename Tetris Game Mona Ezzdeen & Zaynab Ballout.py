1dfe0db537ef752984516972c744d03c03133b3e
zaynab01 <zhb050@usal.edu.lb>
2022-05-17 11:35:32 +0300

    added class figures

--- Tetris Game Mona Ezzdeen & Zaynab Ballout.py
+++ Tetris Game Mona Ezzdeen & Zaynab Ballout.py
@@ -1,8 +1,6 @@
-# importing from environments
 import pygame
 import random
 
-# declare colors
 colors = [
     (0, 0, 0),
     (120, 37, 179),
@@ -13,3 +11,31 @@ colors = [
     (180, 34, 122),
 ]
 
+
+class Figure:
+    x = 0
+    y = 0
+
+    figures = [
+        [[1, 5, 9, 13], [4, 5, 6, 7]],
+        [[4, 5, 9, 10], [2, 6, 5, 9]],
+        [[6, 7, 9, 10], [1, 5, 6, 10]],
+        [[1, 2, 5, 9], [0, 4, 5, 6], [1, 5, 9, 8], [4, 5, 6, 10]],
+        [[1, 2, 6, 10], [5, 6, 7, 9], [2, 6, 10, 11], [3, 5, 6, 7]],
+        [[1, 4, 5, 6], [1, 4, 5, 9], [4, 5, 6, 9], [1, 5, 6, 9]],
+        [[1, 2, 5, 6]],
+    ]
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.type = random.randint(0, len(self.figures) - 1)
+        self.color = random.randint(1, len(colors) - 1)
+        self.rotation = 0
+
+    def image(self):
+        return self.figures[self.type][self.rotation]
+
+    def rotate(self):
+        self.rotation = (self.rotation + 1) % len(self.figures[self.type])
+        
